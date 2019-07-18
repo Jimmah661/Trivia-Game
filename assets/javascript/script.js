2ef1463e7051316f7e4a1bb06ccdbc9aec93706a
James Rudd <james.rudd45@gmail.com>
2019-07-18 14:13:08 +1000

    Now pulling information from the form

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,11 +18,23 @@ var timeControl = {
     }
 }
 
+var resultsTable = [];
+
 $(document).ready(function () {
-    // Timer countdown code
+    // Timer countdown kickoff
     timeControl.intID = setInterval(timeControl.updateTimer, 1000);
 })
 
+$("#quizForm").submit(function (e) {
+    e.preventDefault();
+});
+
 function quizChecker() {
-    alert('Button Clicked!')
+    // alert('Button Clicked!')
+    clearInterval(timeControl.intID)
+    console.log($('#q1:checked').val())
+    console.log($('#q2:checked').val())
+    console.log($('#q3:checked').val())
+    console.log($('#q4:checked').val())
+    console.log(timeControl.timer + " seconds remaining")
 }
