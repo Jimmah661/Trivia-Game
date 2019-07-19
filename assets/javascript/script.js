8d76260aa34e993caaaf30cac38ae7e36fd1bdfc
James Rudd <james.rudd45@gmail.com>
2019-07-19 16:21:57 +1000

    modal  introduced for results display

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,7 +18,7 @@ var timeControl = {
     }
 }
 
-var resultsTable = [];
+var correctGuess = 0;
 
 $(document).ready(function () {
     // Timer countdown kickoff
@@ -30,11 +30,47 @@ $("#quizForm").submit(function (e) {
 });
 
 function quizChecker() {
-    // alert('Button Clicked!')
+    // To get the value of a question use $('#q1:checked').val()
     clearInterval(timeControl.intID)
-    console.log($('#q1:checked').val())
-    console.log($('#q2:checked').val())
-    console.log($('#q3:checked').val())
-    console.log($('#q4:checked').val())
     console.log(timeControl.timer + " seconds remaining")
+    for (i = 1; i < 6; i++) {
+        // $('#q' + i + ':checked').push(resultsTable)
+        // resultsTable.push($('#q' + i + ':checked').val())
+        if ($('#q' + i + ':checked').val() === "1") {
+            correctGuess++
+        }
+    }
+    console.log('You got ' + correctGuess + ' questions correct out of a possible 5')
+    $('#resultsDisplay').html('Congrats, you got ' + correctGuess + " questions right!")
+    $('.modal').css('display', 'block')
 }
+
+
+// WANT TO USE A MODAL WINDOW TO DISPLAY QUIZ RESULTS
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("test");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+// btn.onclick = function () {
+//     modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
